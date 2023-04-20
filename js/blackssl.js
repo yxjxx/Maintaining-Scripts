2b3076c0fcac12b0379738746ca2975bdd3a850d
Jing Yang <yxjxx@users.noreply.github.com>
2023-04-20 17:30:24 +0800

    Update blackssl.js

--- js/blackssl.js
+++ js/blackssl.js
@@ -57,11 +57,13 @@ $httpClient.get({
   let total = res['transfer_enable']
   let used = res['transfer_used']
   let reset = res['days_to_reset']
+  let days_to_reset = Number(reset) ? Number(reset) : 1
+  let remain_per_day = remain / days_to_reset
   let date = new Date(res['expired_at'] * 1000)
   let month = date.getMonth() + 1
   let expired_at = "到期: " + date.getFullYear() + "/" + month + "/" + date.getDate()
   let remain = Number(total) - Number(used)
-  let remain_str = "剩余: " + humanize_byte(remain, 1) + " | " + remain/Number(reset) + ' 每天'
+  let remain_str = "剩余: " + humanize_byte(remain, 2) + " | " + humanize_byte(remain_per_day, 1) + ' 每天'
   if (!total) {
     $done({
       style: "error",
@@ -70,7 +72,7 @@ $httpClient.get({
   } else {
     $done({
       title: "用量: "+ humanize_byte(used, 2) + " | " + humanize_byte(total, 0) + "   "+nowtime(),
-      content: remain_str + "\n重置: 剩余 " + reset + " 天 \n" + expired_at,
+      content: remain_str + "\n重置: 剩余 " + reset + " 天 | " + expired_at,
       icon: 'externaldrive.connected.to.line.below',
 		  'icon-color':'#9a7ff7'
     });
