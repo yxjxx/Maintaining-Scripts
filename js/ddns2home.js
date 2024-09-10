ed5e57612576f8cd233a875ab3f09d77358570e2
Jing Yang <yxjxx@users.noreply.github.com>
2024-09-10 19:32:03 +0800

    Update ddns2home.js

--- js/ddns2home.js
+++ js/ddns2home.js
@@ -1,5 +1,7 @@
 if ($network.wifi.ssid === 'CU_pitaya') {
     $done({ address: '192.168.0.100' })
+} else if ($network.wifi.ssid === 'Inspire Creativity') {
+    $done({ server: '223.5.5.5' })
 } else {
     $done({})
 }
